b51121388aa6510bafbd67c8a3305a38cd90318d
SkylineCodes <onakoyak@gmail.com>
2023-12-31 08:30:16 +0100

    Base URL env bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -354,7 +354,7 @@ const Home = () => {
                 <div className='mt-16 mb-12 flow-root sm:mt-24'>
                   <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                     <ReactPlayer
-                      url={`${process.env.baseURL}/dashboard-preview.mp4`}
+                      url={`${process.env.NEXT_PUBLIC_BASE_URL}/dashboard-preview.mp4`}
                       muted={true}
                       playing={true}
                       loop={true}
